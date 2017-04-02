bad35d5de7b8ebadd2478c653fc3205c06b4feb5
Vivek Bezawada <Vivek Bezawada>
2017-04-02 09:14:56 +0530

    Basic APIs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var dbConnection = 'mongodb://epskip:smalley009@ds149030.mlab.com:49030/aircute'
 
 var app = express();
 
@@ -14,11 +16,67 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req,res,next){
 
-	res.render('main', { layout:'layouts/admin', title: 'Express'});
+// Listener and Database Connection
+app.listen(80,function(){
+	MongoClient.connect(dbConnection, function(err, database) {
+		db = database
+	  if(!err) {
+			console.log("Database is COnnected and Application is running...")    
+	  }
+	});
 })
 
-app.listen(8080,function(){
-	console.log("serving")
+
+
+app.get('/', function(req,res){
+	res.send("HOMEPAGE");
+	//res.render('main', { layout:'layouts/admin', title: 'Express'});
 })
+
+//GET Request - Signup for the Client
+app.get('/signUpMediaOwner', function(req,res){
+	res.render('signup', { layout:null, title: 'Signup | Aircute'});
+})
+
+// POST Request - Signup for the Client
+app.post('/signUpMediaOwner', function(req,res){
+	/*
+	var mediaName
+	phoneNumber
+	emailAddress
+	Address
+	username
+	password
+	confirmPassword
+	mediaLogo
+	*/
+})
+
+//GET Request - Signup User
+app.get('/signUpUser', function(req,res){
+	res.render('signup', { layout:null, title: 'Signup | Aircute'});
+})
+
+//POST Request - Signup User
+app.post('/signUpUser', function(req,res){
+	/*
+	var username
+	password
+	emailAddress
+	*/
+})
+
+// GET Request - Login Media owner
+app.get('/signinMediaOwner', function(req,res){
+	res.render('signin', { layout:null, title: 'Signin | Aircute'});
+})
+
+//POST Request - Login Media Owner
+app.post('/signinMediaOwner', function(req,res){
+	/*
+	var username
+	password
+	*/
+})
+
