487e7be08f319d5351bce0b19daf895f3c9a775c
VivekBezawada <bezawadavivek@gmail.com>
2017-07-01 10:50:43 +0530

    Response Header

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.engine('html', require('hbs').__express);
 
 app.use(function(req, res, next) {
 
-  res.header("Access-Control-Allow-Methods", GET,PUT,POST,DELETE);
+  res.header("Access-Control-Allow-Methods", "GET","PUT","POST","DELETE");
   res.header("Access-Control-Allow-Origin", "http://localhost:27690");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
